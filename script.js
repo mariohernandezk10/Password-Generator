f1e85b78d8c5060a499af74122e728461afac332
Mario Hernandez <mariohernandezk10@gmail.com>
2020-10-26 17:00:54 -0600

    added randomLowerCase function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowCletter = "abcdefghijklmnopqrstuvwxyz"
+var upCletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var Num = "0123456789"
+var symb = "({';[])-*&^%$#@!~`<>,."
+
+function randomLowerCase() {
+  console.log(lowCletter[Math.floor(Math.random() * lowCletter.length)]);
+}
+
+function randomUpperCase() {
+  console.log(upCletter[Math.floor(Math.random() * 26)]);
+}
 
 // Write password to the #password input
 function writePassword() {
